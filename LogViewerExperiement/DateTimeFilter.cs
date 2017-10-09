fb27fd3e1b2bf1dc67d4ef85e15fbc0e7ce06298
Raj Jindel <Rajnesh.Jindel@splashdamage.com>
2017-10-09 16:00:00 +0100

    Hopefully the minimum needed to create the log tool project

---
+++ LogViewerExperiement/DateTimeFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace LogViewerExperiement
+{
+    class DateTimeFilter
+    {
+        //const string LOG_LINE_REGEX = @"^\[(?<year>\d*)\.(?<month>\d*)\.(?<day>\d*)-(?<hour>\d*)\.(?<minute>\d*)\.(?<second>\d*):(?<milisecond>\d*)\]\[(?<thing>\s*\d*)\](?<category>\w+):(?<message>.*)";
+        //const string LOG_LINE_REGEX = @"^\[(?<year>\d+)\.(?<month>\d+)\.(?<day>\d+)-(?<hour>\d+)\.(?<minute>\d+)\.(?<second>\d+)\:(?<milisecond>\d+)\]\[(?<frame>\s*d*)\]";
+        const string LOG_LINE_REGEX = @"^\[(?<year>\d+)\.(?<month>\d+)\.(?<day>\d+)-(?<hour>\d+)\.(?<minute>\d+)\.(?<second>\d+)\:(?<milisecond>\d+)\]";
+
+        Regex m_parser;
+        public DateTimeFilter() {
+            m_parser = new Regex(LOG_LINE_REGEX, RegexOptions.Singleline | RegexOptions.Compiled);
+        }
+        public TextBlock Process(String Text)
+        {
+            TextBlock tb = new TextBlock();
+            Match match = m_parser.Match(Text);
+            if (match.Success)
+            {
+                tb.Inlines.Add(new Run(match.Value) { FontWeight = FontWeights.Bold });
+                tb.Inlines.Add(Text.Substring(match.Index + match.Length));
+            }
+            else
+            {
+                tb.Inlines.Add(Text);
+            }
+
+            return tb;
+        }
+    }
+}
---
+++ LogViewerExperiement/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace LogViewerExperiement
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ShowOpenFileDialog(Object sender, RoutedEventArgs args)
+        {
+            var fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = false;
+            fileDialog.Filter = "Log files (*.log)|*.log";
+
+            if(fileDialog.ShowDialog() == true)
+            {
+                OpenFile(fileDialog.FileName);
+            }
+        }
+        async void OpenFile(string Filename)
+        {
+            {
+                //MessageBox.Show("NotImplemented! Actually open selected file: " + fileDialog.FileName);
+                var stream = new StreamReader(Filename);
+
+                var Log = new List<TextBlock>();
+                DateTimeFilter filter = new DateTimeFilter();
+                string line;
+                while ((line = await stream.ReadLineAsync()) != null)
+                {
+                    //TextBlock tb = new TextBlock();
+                    //tb.Inlines.Add(line);
+                    TextBlock tb = filter.Process(line);
+                    Log.Add(tb);
+                }
+                LogText.ItemsSource = Log;
+            }
+
+        }
+
+        private void FindClicked(object sender, RoutedEventArgs e)
+        {
+
+            foreach (TextBlock item in LogText.ItemsSource)
+            {
+                if (item.Text.ToString().Contains(SearchText.Text))
+                {
+                    item.Select(SearchText.Text);
+                    LogText.ScrollIntoView(item);
+                    LogText.SelectedItem = item;
+                    break;
+                }
+
+            }
+            //if(LogText.Text.Length > SearchText.Text.Length)
+            //LogText.Items.
+            //{
+            //    int Index = LogText.Text.IndexOf(SearchText.Text);
+            //    double ScrollToLocation = (double)Index ;
+            //    LogText.ScrollToVerticalOffset(ScrollToLocation);
+            //}
+        }
+    }
+}
