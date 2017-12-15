9d8a4191a8469eeaca1da39332f40c563aff6b2b
Rajnesh.Jindel <Rajnesh.Jindel@BLUE-SAOLA>
2017-12-15 18:23:16 +0000

    Move highlighting and selecting a found item into their own function When finding, unselect and remove highlight from previously selected item. Add Find previous function Replace DateTimeFilter with generic Highlight Filter.

---
+++ LogViewerExperiement/HighlightFilter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Controls;
+
+namespace LogViewerExperiement
+{
+    class HighlightFilter
+    {
+        string m_SearchPattern;
+
+        Run m_Style;
+        Regex m_Parser;
+
+        public HighlightFilter(string searchPattern, Run HighlightStyle)
+        {
+            m_SearchPattern = searchPattern;
+            m_Parser = new Regex(m_SearchPattern, RegexOptions.Singleline | RegexOptions.Compiled);
+            m_Style = HighlightStyle;
+        }
+
+        public TextBlock Process(string Text)
+        {
+            //TODO: rename
+            TextBlock tb = new TextBlock();
+
+            Match match = m_Parser.Match(Text);
+            if (match.Success)
+            {
+                Run HighlightedText = new Run();
+                HighlightedText.Foreground = m_Style.Foreground;
+                HighlightedText.FontWeight = m_Style.FontWeight;
+                HighlightedText.Background = m_Style.Background;
+                //HighlightedText. = m_Style.;
+                HighlightedText.Text = match.Value;
+                tb.Inlines.Add(HighlightedText);
+                tb.Inlines.Add(Text.Substring(match.Index + match.Length));
+            }
+            else
+            {
+                tb.Inlines.Add(Text);
+            }
+            return tb;
+        }
+    }
+}
--- LogViewerExperiement/MainWindow.xaml.cs
+++ LogViewerExperiement/MainWindow.xaml.cs
@@ -44,7 +44,12 @@ namespace LogViewerExperiement
                 var stream = new StreamReader(Filename);
 
                 var Log = new List<TextBlock>();
-                DateTimeFilter filter = new DateTimeFilter();
+                //DateTimeFilter filter = new DateTimeFilter 
+
+                //const string UE4_TIMESTAMP= @"^\[(?<year>\d+)\.(?<month>\d+)\.(?<day>\d+)-(?<hour>\d+)\.(?<minute>\d+)\.(?<second>\d+)\:(?<milisecond>\d+)\]";
+                const string UE3_TIMESTAMP = @"^\[(\d+)\.(\d+)\:(?<year>\d+)\.(?<month>\d+)\.(?<day>\d+)-(?<hour>\d+)\.(?<minute>\d+)\.(?<second>\d+)]";
+                HighlightFilter filter = new HighlightFilter(UE3_TIMESTAMP, new Run() { FontWeight = FontWeights.Bold });
+
                 string line;
                 while ((line = await stream.ReadLineAsync()) != null)
                 {
@@ -56,32 +61,122 @@ namespace LogViewerExperiement
 
         }
 
+        private void Select(TextBlock item)
+        {
+            LogText.ScrollIntoView(item);
+            LogText.SelectedItem = item;
+        }
+        private void Deselect()
+        {
+            RemoveHighlight(LogText.SelectedItem as TextBlock, Brushes.Yellow);
+            LogText.SelectedItem = null;
+        }
+
+        private void HighlightText(TextBlock item, string text, Brush brush)
+        {
+            foreach (Run line in item.Inlines)
+            {
+                int index = line.Text.IndexOf(SearchText.Text);
+                if (index != -1)
+                {
+                    var newItem = new Run(line.Text.Substring(0, index));
+                    item.Inlines.InsertBefore(line, newItem);
+                    newItem = new Run(SearchText.Text) { Background = brush };
+                    item.Inlines.InsertBefore(line, newItem);
+                    newItem = new Run(line.Text.Substring(index + SearchText.Text.Length));
+                    item.Inlines.InsertBefore(line, newItem);
+                    item.Inlines.Remove(line);
+                    break;
+                }
+            }
+        }
+
+        private void RemoveHighlight(TextBlock item, Brush brush)
+        {
+            Run PreviousLine = null;
+            for(int i = 0; i < item.Inlines.Count; i++)
+            {
+                Run line = item.Inlines.ElementAt(i) as Run;
+                if (line.Background == brush)
+                {
+                    string text = string.Empty;
+                    if(PreviousLine != null)
+                    {
+                        text = PreviousLine.Text;
+                        item.Inlines.Remove(PreviousLine);
+                    }
+                    text += line.Text;
+                    if (i + 1 < item.Inlines.Count)
+                    {
+                        Run next = item.Inlines.ElementAt(i + 1) as Run;
+                        text += next.Text;
+                        item.Inlines.Remove(next);
+                    }
+                    item.Inlines.InsertBefore(line, new Run(text));
+                    item.Inlines.Remove(line);
+                    break;
+                }
+                PreviousLine = line;
+            }
+        }
+
+
         private void FindClicked(object sender, RoutedEventArgs e)
         {
             foreach (TextBlock item in LogText.ItemsSource)
             {
-                if (item.Text.ToString().Contains(SearchText.Text))
+                if(item == LogText.SelectedItem)
+                {
+                    Deselect();
+                    continue;
+                }
+                if(LogText.SelectedItem != null)
+                {
+                    continue;
+                }
+
+                //if (item.Text.ToString().Contains(SearchText.Text))
                 {
                     foreach(Run r in item.Inlines)
                     {
                         int index = r.Text.IndexOf(SearchText.Text);
                         if(index != -1)
                         {
-                            var newItem = new Run(r.Text.Substring(0, index));
-                            item.Inlines.InsertAfter(r, newItem);
-                            newItem = new Run(SearchText.Text) { Background = Brushes.Yellow };
-                            item.Inlines.InsertAfter(r, newItem);
-                            newItem = new Run(r.Text.Substring(index + SearchText.Text.Length));
-                            item.Inlines.InsertAfter(r, newItem);
-                            item.Inlines.Remove(r);
+                            HighlightText(item, SearchText.Text, Brushes.Yellow);
+                            Select(item);
                             break;
                         }
                     }
-                    LogText.ScrollIntoView(item);
-                    LogText.SelectedItem = item;
-                    break;
                 }
 
+            }
+        }
+
+        private void FindPrevious(object sender, RoutedEventArgs e)
+        {
+            int i;
+            if (LogText.SelectedItem != null)
+            {
+                i = LogText.SelectedIndex - 1;
+                Deselect();
+            }
+            else
+            {
+                i = LogText.Items.Count - 1;
+            }
+            for (; i >= 0 ; --i)
+            {
+                TextBlock item = LogText.Items.GetItemAt(i) as TextBlock;
+                foreach (Run r in item.Inlines)
+                {
+                    int index = r.Text.IndexOf(SearchText.Text);
+                    if (index != -1)
+                    {
+                        HighlightText(item, SearchText.Text, Brushes.Yellow);
+                        Select(item);
+                        return;
+                    }
+                }
             }
         }
 
